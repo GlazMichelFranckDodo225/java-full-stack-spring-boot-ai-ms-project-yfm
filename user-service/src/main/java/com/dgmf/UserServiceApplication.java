a0930a4c371cb682f41c7653c28a47fde9b4ad84
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2025-11-11 06:23:51 +0100

    Setting Up User Service - 2

--- user-service/src/main/java/com/dgmf/UserServiceApplication.java
+++ user-service/src/main/java/com/dgmf/UserServiceApplication.java
@@ -1,4 +1,4 @@
-package com.dgmf.user_service;
+package com.dgmf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- user-service/src/test/java/com/dgmf/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/dgmf/UserServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.dgmf.user_service;
+package com.dgmf;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
